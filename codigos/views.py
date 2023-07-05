af4214e6af2e7c59a537776f56e8b53ceb9fcbdf
facundo <facundorivero1991@gmail.com>
2023-07-05 12:16:29 -0300

    detalles views.py

--- codigos/views.py
+++ codigos/views.py
@@ -9,35 +9,65 @@ def consulta(request):
     if request.method == "GET":
         context = {"dato":""}
     elif request.method == "POST":
-        dato = (request.POST["valor"])
-        resultado = Codigos.objects.get(id= dato)
-        context = {"dato":resultado.codigo}
+        valor = (request.POST["valor"])
+        try: 
+            resultado = Codigos.objects.get(id= valor)
+
+            if resultado.codigo == "RECICLABLE":
+                clase_css = "bg-success"
+            elif resultado.codigo == "NO RECICLABLE":
+                clase_css = "bg-danger"
+            else:
+                clase_css = ""
+
+            context = {"respuesta":resultado.codigo, "clase_css": clase_css}
+
+        except Codigos.DoesNotExist:
+            context = {"respuesta":"CODIGO NO ENCONTRADO"}
+    
+
     else:
-        context = {"dato":"ERROR AL ENVIAR LA SOLICITUD"}
+        context = {"respuesta":"ERROR AL ENVIAR LA SOLICITUD"}
 
     return render(request,"consulta.html",context)
 
 def agregar(request):
     if request.method == "GET":
-        context = {"dato":""}
+        context = {"respuesta":""}
     elif request.method == "POST":
-        id = request.POST.get("id") 
-        tipo = request.POST.get("tipo") 
-        nuevo_codigo = Codigos(id,tipo)
-        nuevo_codigo.save()
-        context = {"dato":"Agregado correctamente"}
+        valor = request.POST.get("id") 
+        try: 
+            resultado = Codigos.objects.get(id= valor)
+            resultado = False
+        
+        except Codigos.DoesNotExist:
+            resultado = True
+        
+        if resultado:
+            tipo = request.POST.get("tipo") 
+            nuevo_codigo = Codigos(valor,tipo)
+            nuevo_codigo.save()
+            context = {"respuesta":"AGREGADO CORRECTAMENTE"}
+        else:
+            context = {"respuesta":"CODIGO YA EXISTENTE"}
     else:
-        context = {"dato":"ERROR AL ENVIAR LA SOLICITUD"}
+        context = {"respuesta":"ERROR AL ENVIAR LA SOLICITUD"}
 
     return render(request,"agregar.html",context)
 
 def eliminar(request):
     if request.method == "GET":
-        context = {"dato":""}
+        context = {"respuesta":""}
     elif request.method == "POST":
-        #LEER BASE DE DATOS
-        context = {"dato":"Elimiando correctamente"}
+        dato = request.POST.get("id") 
+        try: 
+            eliminar_codigo = Codigos.objects.get(id= dato)
+            eliminar_codigo.delete()
+            context = {"respuesta":"ELIMINADO CORRECTAMENTE"}
+
+        except Codigos.DoesNotExist:
+            context = {"respuesta":"NO EXITE EL CODIGO"}
     else:
-        context = {"dato":"ERROR AL ENVIAR LA SOLICITUD"}
+        context = {"respuesta":"ERROR AL ENVIAR LA SOLICITUD"}
 
     return render(request,"eliminar.html",context)
